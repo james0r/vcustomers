47da79e4dd97cf34d95bdf4426f9ebb8c97833d9
James <james.auble@gmail.com>
2018-02-20 21:33:09 -0800

    Added table and dynamic links to display response of customers

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="customers container">
     <h1 class="page-header">Manage Customers</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" :key="customer.id">
+          <td>{{ customer.first_name}}</td>
+          <td>{{ customer.last_name}}</td>
+          <td>{{ customer.email}}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -16,7 +34,7 @@ export default {
     fetchCustomers() {
       this.$http.get('http://slimapp/api/customers')
       .then(function(response){
-        console.log(response.body);
+        this.customers = response.body;
       });
     }
   },
